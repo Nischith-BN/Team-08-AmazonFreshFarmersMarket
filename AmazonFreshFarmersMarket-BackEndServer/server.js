9f26fb9dd1f5c0ed4fcbe601aba8e7b0298205f4
nischith-bn <nischithbedachuvaly.nataraja@sjsu.edu>
2016-04-19 20:15:37 -0700

    Integrated signup and login modules
    Integrated signup and login modules

---
+++ AmazonFreshFarmersMarket-BackEndServer/server.js
@@ -0,0 +1,60 @@
+var amqp = require('amqp')
+, util = require('util');
+
+var login = require('./routes/login')
+
+var cnn = amqp.createConnection({host:'127.0.0.1'});
+
+cnn.on('ready', function(){
+	
+	cnn.queue('createAccount_queue', function(q){
+		q.subscribe(function(message, headers, deliveryInfo, m){
+			console.log("in server cnn.subscribe " );
+			util.log(util.format( deliveryInfo.routingKey, message));
+			util.log("Message: "+JSON.stringify(message));
+			util.log("DeliveryInfo: "+JSON.stringify(deliveryInfo));
+			login.handle_createAccount_request(message, function(err,res){
+
+				//return index sent
+				cnn.publish(m.replyTo, res, {
+					contentType:'application/json',
+					contentEncoding:'utf-8',
+					correlationId:m.correlationId
+				});
+			});
+		});
+	});
+	
+	cnn.queue('signup_queue', function(q){
+		q.subscribe(function(message, headers, deliveryInfo, m){
+			console.log("in server cnn.subscribe " );
+			util.log(util.format( deliveryInfo.routingKey, message));
+			util.log("Message: "+JSON.stringify(message));
+			util.log("DeliveryInfo: "+JSON.stringify(deliveryInfo));
+			login.handle_signup_request(message, function(err,res){
+				cnn.publish(m.replyTo, res, {
+					contentType:'application/json',
+					contentEncoding:'utf-8',
+					correlationId:m.correlationId
+				});
+			});
+		});
+	});
+	
+	cnn.queue('login_queue', function(q){		
+		q.subscribe(function(message, headers, deliveryInfo, m){
+			util.log(util.format( deliveryInfo.routingKey, message));
+			util.log("Message: "+JSON.stringify(message));
+			util.log("DeliveryInfo: "+JSON.stringify(deliveryInfo));
+			login.handle_login_request(message, function(err,res){
+
+				//return index sent
+				cnn.publish(m.replyTo, res, {
+					contentType:'application/json',
+					contentEncoding:'utf-8',
+					correlationId:m.correlationId
+				});
+			});
+		});
+	});
+});
