7eb496b4647c7f5e4f117bc766c3aedaddc643fc
jainbhargav <jainbhargav01@gmail.com>
2016-04-26 02:41:34 -0700

    new Product and Farmer API changes done

--- AmazonFreshFarmersMarket-BackEndServer/routes/farmer.js
+++ AmazonFreshFarmersMarket-BackEndServer/routes/farmer.js
@@ -8,7 +8,45 @@ exports.handle_listFarmers_request = function(msg, callback){
 };
 
 exports.handle_amendFarmerDetails_request = function(msg, callback){
+	var farmerId =msg.farmerId;
+	var firstName =msg.firstName;
+	var lastName =msg.lastName;
+	var address =msg.address;
+	var city =msg.city;
+	var state =msg.state;
+	var zipCode =msg.zipCode;
+	var phoneNumber =msg.phoneNumber;
+	var password =msg.password;
+	//req.param("productDescription");
 
+	var sqlResult = null;
+	console.log("exports.handle_amendFarmerDetails_request ~~~~~~~~~~~");
+	var json_response = {};
+	
+	var query = "Update amazonfresh.farmer set firstname ='"+firstName+"',lastname = '"+lastName+"',address = '"+address+"',city = '"+city+"',state = '"+state+"',zip_code = '"+zipCode+"',city = '"+city+"' where farmer_id ='" + farmerId +"'";
+	
+console.log("Sql Query:"+query);
+	
+	mysql.fetchData(function(error, results) {
+		console.log(results);
+		if(error)
+		{
+			json_response ={
+					"statusCode" : 401,
+					"statusMessage" : "Error occured while updating the product details"
+			};
+		}
+		else
+		{	
+			
+			json_response ={
+					"statusCode" : 200,
+					"results" : results
+			};
+		}
+		callback(null, json_response);
+	}, query);
+	
 };
 
 exports.handle_searchFarmer_request = function(msg, callback){
@@ -16,5 +54,70 @@ exports.handle_searchFarmer_request = function(msg, callback){
 };
 
 exports.handle_fetchFarmerDetails_request = function(msg, callback){
+
+	
+	var farmerId = msg.farmerId;
+	
+	var sqlResult = null;
+	console.log("exports.handle_viewProduct_request ~~~~~~~~~~~");
+	var json_response = {};
+
+	var query = "select * from amazonfresh.farmer where farmer_id ='" + farmerId +"'"; 
+	
+	console.log("Sql Query:"+query);
+
+	mysql.fetchData(function(error, results) {
+		if(error)
+		{
+			console.log("In error part");
+			console.log("In error part"+error);
+			json_response ={
+					"statusCode" : 401,
+					"statusMessage" :  "Unexpected error occured"
+			};
+		}
+		else
+		{	
+			console.log("Before mongo query");
+			sqlResult = results;
+			
+			mongo.connect(mongoURL, function(){
+				var coll = mongo.collection('farmer_details');
+				console.log("firing mongo query for farmer" + farmerId);
+				coll.findOne({farmer_id: farmerId}, function(err, results){
+					
+					if (err) 
+					{
+						json_response ={
+								"statusCode" : 401,
+								"statusMessage" : "Unexpected error occured"
+						};
+					} else if(results) {
+						console.log(results);
+						
+							json_response ={
+									"statusCode" : 200,
+									"statusMessage" : "Fetched result successfully",
+									"mongoResult" : results,
+									"sqlResult" : sqlResult
+							};
+							callback(null, json_response);
+						}
+					 else {
+						json_response ={
+								"statusCode" : 403,
+								"statusMessage" : "Invalid farmer",
+								"results" : results
+						};
+						callback(null, json_response);
+					}
+					
+				});
+			});	
+			
+			
+		}
+	
+	}, query);
 
 };
--- AmazonFreshFarmersMarket-BackEndServer/routes/login.js
+++ AmazonFreshFarmersMarket-BackEndServer/routes/login.js
@@ -1,16 +1,13 @@
-var bcrypt = require('bcrypt-nodejs');
-var mysqlModule = require('mysql');
+var s = require('mysql');
 var mongo = require("./mongo");
 var mysql = require("./mysql");
 var mongoURL = "mongodb://localhost:27017/amazonFreshFarmersMarket";
 
 exports.handle_login_request = function(msg, callback){
-	console.log("-----exports.handle_login_request-----");
 	var json_response = {};
 	var email = msg.email;
 	var password = msg.password;
 	console.log("email " + email +" password "+ password);
-
 	mongo.connect(mongoURL, function(){
 		var coll = mongo.collection('login_credentials');
 		console.log('email ' + email + ' password ' + password);
@@ -22,7 +19,6 @@ exports.handle_login_request = function(msg, callback){
 						"statusCode" : 401,
 						"statusMessage" : "Unexpected error occured"
 				};
-				callback(null, json_response);
 			} else if(results) {
 				if(results.approved == 0)
 				{
@@ -31,29 +27,12 @@ exports.handle_login_request = function(msg, callback){
 							"statusMessage" : "Approval pending",
 							"results" : results
 					};
-					callback(null, json_response);
-				} else if(results.approved == 2){
-					json_response ={
-							"statusCode" : 403,
-							"statusMessage" : "Request rejected by administration",
-							"results" : results
-					};
-					callback(null, json_response);
-				} else if(results.approved == 3){
-					json_response ={
-							"statusCode" : 403,
-							"statusMessage" : "Account deleted",
-							"results" : results
-					};
-					callback(null, json_response);
-				}			
-				else {
+				} else {
 					json_response ={
 							"statusCode" : 200,
 							"statusMessage" : "Login Successful",
 							"results" : results
 					};
-					callback(null, json_response);
 				}
 			} else {
 				json_response ={
@@ -61,73 +40,45 @@ exports.handle_login_request = function(msg, callback){
 						"statusMessage" : "Invalid login credentials",
 						"results" : results
 				};
-				callback(null, json_response);
-			}			
+			}
+			callback(null, json_response);
 		});
 	});	
 };
 
 exports.handle_adminLogin_request = function(msg, callback){
-	console.log("-----exports.handle_adminLogin_request-----");
 	var json_response = {};
 	var email = msg.email;
 	var password = msg.password;
 
-	var query = "select * from amazonfresh.administrator where email='"+ email +"'";
-	console.log(query);
-	mysql.fetchData(function(error, results) {
-		if(error)
-		{
-			console.log(error);
-			json_response ={
-					"statusCode" : 401,
-					"results" : error.code
-			};
-			callback(null, json_response);
-		}
-		else
-		{		
-			console.log(results);
-			if(results.length <= 0)
+	mongo.connect(mongoURL, function(){
+		var coll = mongo.collection('login_credentials');
+		coll.findOne({email: email}, function(err, results){
+			if (err) 
 			{
 				json_response ={
-						"statusCode" : 403,
-						"statusMessage" : "Invalid email id"
+						"statusCode" : 401,
+						"statusMessage" : "Unexpected error occured"
 				};
-				callback(null, json_response);
+			} else if(results) {				
+				json_response ={
+						"statusCode" : 200,
+						"statusMessage" : "Login Successful",
+						"results" : results
+				};				
+			} else {
+				json_response ={
+						"statusCode" : 403,
+						"statusMessage" : "Invalid login credentials",
+						"results" : results
+				};
 			}
-			else
-			{
-				bcrypt.compare(password, results[0].password, function(err,correctPassword) {
-
-					console.log ("//////////////// " + correctPassword);
-					console.log ("password " + password);
-					console.log ("password " + results[0].password);
-
-
-
-					if(correctPassword){						
-						json_response ={
-								"statusCode" : 200,
-								"results" : results
-						};
-						callback(null, json_response);
-					}
-					else{                      
-						json_response ={
-								"statusCode" : 403,
-								"statusMessage" : "Invalid login credentials"
-						};
-						callback(null, json_response);                     
-					}
-				});	
-			}			
-		}		
-	}, query);
+			callback(null, json_response);
+		});
+	});	
 };
 
 exports.handle_createAccount_request = function(msg, callback){
-	console.log("-----exports.handle_createAccount_request-----");
 	var json_response = {};
 	var email = msg.email;
 
@@ -143,7 +94,6 @@ exports.handle_createAccount_request = function(msg, callback){
 						"statusCode" : 401,
 						"statusMessage" : "Unexpected error occured"
 				};
-				callback(null, json_response);
 			}
 			else if (results) {	
 				console.log("successsssssssssssssssss");
@@ -151,25 +101,23 @@ exports.handle_createAccount_request = function(msg, callback){
 						"statusCode" : 403,
 						"statusMessage" : "Email Already Exists",
 						"results" : results
-				};	
-				callback(null, json_response);
+				};			
 			} else {
 				json_response ={
 						"statusCode" : 200,
 						"statusMessage" : "Doesnot Exist",
 						"results" : results
 				};
-				callback(null, json_response);
 			}
+			callback(null, json_response);
 		});
 	});	
 };
 
 exports.handle_saveCardDetails_request = function(msg, callback){
-	console.log("-----exports.handle_saveCardDetails_request-----");
 	var json_response = {};
 
-	var email= msg.email;
+	/*var email= msg.email;
 	var firstName = msg.firstName;
 	var lastName= msg.lastName;
 	var password= msg.password;
@@ -184,191 +132,164 @@ exports.handle_saveCardDetails_request = function(msg, callback){
 	var cardNumber = msg.cardNumber;
 	var cardHolderName = msg.cardHolderName;
 	var cardExpirationMonth = msg.cardExpirationMonth;
-	var cardExpirationYear = msg.cardExpirationYear;	
-	var approved = 0;
+	var cardExpirationYear = msg.cardExpirationYear;*/	
 
-	var id = '100000000';
+	var customerId = generateSSN(10000,999999999);
 
-	var query = "select max(customer_id) as max_id from amazonfresh.customer";
+	var email= 'aaa@a';
+	var firstName = "nichuuuuuuuuuuuu";
+	var lastName= "n";
+	var password= "12345";
+	var category= "farmer";
 
-	mysql.fetchData(function(error, results) {
-		if(error)
-		{
-			json_response ={
-					"statusCode" : 401,
-					"results" : error.code
-			};
-			callback(null, json_response);
-		}
-		else
-		{		
-			if(results.length > 0)
+	var streetAddress= "a";
+	var city= "a";
+	var state= "a";
+	var zipCode= "12345";
+	var phoneNumber= "1234567899";
+
+	var cardNumber = "123";
+	var cardHolderName = "as";
+	var cardExpirationMonth = "01";
+	var cardExpirationYear = "2016";
+	var isApproved = "0";
+
+	console.log("VVVVVVVVVVVVVVVVVVVVVV");
+	mongo.connect(mongoURL, function(){
+		var coll = mongo.collection('login_credentials');
+		coll.insertOne({email: email,password:password,category:category,customerId:customerId,isApproved:isApproved}, function(err, results){
+			console.log("XXXXXXXXXXXXXXXXXXXXXXXXXXX");
+			console.log(results);
+			if (err) 
 			{
-				id = results[0].max_id + 1;
-			}
-
-			var insertQuery = "insert into amazonfresh.customer (customer_id, firstname, lastname, address, city, " +
-			"state, zip_code, phone_number, email, password,credit_card_no, card_holder_name, expiration_month, expiration_year, approved) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
-
-			var inputParams = [id,firstName,lastName,streetAddress,city,state,zipCode,phoneNumber,email,password,cardNumber,cardHolderName,cardExpirationMonth,cardExpirationYear, approved];
-			console.log(inputParams);
-
-			var insertQuery = mysqlModule.format(insertQuery, inputParams);
-
-			mysql.fetchData(function(err,results){
-				console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-				console.log(err);
-				console.log(results);		
-
-				if(err){
-					console.log("//////////////////");
-					console.log(err.code);
-					json_response ={
-							"statusCode" : 401,
-							"statusMessage" : err.code
-					};
-					callback(null, json_response);
-				}
-				else 
+				json_response ={
+						"statusCode" : 401,
+						"statusMessage" : "Unexpected error occured"
+				};
+			} else if(results) {
+				if(results.approved == 0)
 				{
 					json_response ={
 							"statusCode" : 200,
-							"statusMessage" : "Signup Successful"
+							"statusMessage" : "Signup Successful",
+							"results" : results
 					};
-					callback(null, json_response);
-				}  
-			},insertQuery);
+				} 
+			}
+			callback(null, json_response);
+		});
+	});
+	
+	
+	
+	
+	/*var query = "insert into amazonfresh.customer (customer_id, firstname, lastname, address, city, " +
+	"state, zip_code, phone_number, email, credit_card_no, card_holder_name, expiration_month, expiration_year, isApproved) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+
+	console.log(query);
+	var inputParams = [customerId,firstName,lastName,streetAddress,city,state,zipCode,phoneNumber,email,cardNumber,cardHolderName,cardExpirationMonth,cardExpirationYear,0];
+	console.log(inputParams);
+
+	var insertQuery = mysqlModule.format(query, inputParams);
+	console.log(insertQuery);
+	mysql.fetchData(function(err,results){
+		console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		console.log(err);
+		console.log(results);		
+		
+		if(err){
+			console.log("//////////////////");
+			console.log(err.code);
+			json_response ={
+					"statusCode" : 401,
+					"statusMessage" : err.code
+			};
 		}
-	}, query);
+		else 
+		{
+			mongo.connect(mongoURL, function(){
+				var coll = mongo.collection('login_credentials');
+				coll.insertOne({email: email,password:password,category:category,customerId:customerId,isApproved:0}, function(err, results){
+					console.log("XXXXXXXXXXXXXXXXXXXXXXXXXXX");
+					console.log(results);
+					if (err) 
+					{
+						json_response ={
+								"statusCode" : 401,
+								"statusMessage" : "Unexpected error occured"
+						};
+					} else if(results) {
+						if(results.approved == 0)
+						{
+							json_response ={
+									"statusCode" : 200,
+									"statusMessage" : "Signup Successful",
+									"results" : results
+							};
+						} 
+					}
+					callback(null, json_response);
+				});
+			});
+		}  
+	},insertQuery);*/
+	callback(null, json_response);
 };
 
 exports.handle_saveFarmerDetails_request = function(msg, callback){
 	var json_response = {};
+	var email = msg.email;
 
-	var email= msg.email;
-	var firstName = msg.firstName;
-	var lastName= msg.lastName;
-	var password= msg.password;
-	var category= msg.category;
+	//generate ssn
+	//check category
+	//save data to sqldb - customer or farmer
+	//save data to mongodb
 
-	var streetAddress= msg.streetAddress;
-	var city= msg.city;
-	var state= msg.state;
-	var zipCode= msg.zipCode;
-	var phoneNumber= msg.phoneNumber;
 
-	var image = msg.image;
-	var video = msg.video;
-	var description = msg.description;	
-	var approved = 0;
+	/*mongo.connect(mongoURL, function(){
+		var coll = mongo.collection('login_credentials');
 
-	var id = '100000000';
 
-	var query = "select max(farmer_id) as max_id from amazonfresh.farmer";
-
-	mysql.fetchData(function(error, results) {
-		if(error)
-		{
-			json_response ={
-					"statusCode" : 401,
-					"results" : error.code
-			};
-			callback(null, json_response);
-		}
-		else
-		{		
-			if(results.length > 0)
-			{
-				id = results[0].max_id + 1;
+		coll.findOne({ email: email }, function(err, results){
+			console.log("////////////////////////////////");
+			console.log(results);
+			if(err){
+				console.log("failureeeeeeeeeeeeeeeeeeeeeeee");
+				json_response ={
+						"statusCode" : 401,
+						"statusMessage" : "Unexpected error occured"
+				};
 			}
+			else if (results) {	
+				console.log("successsssssssssssssssss");
+				json_response ={
+						"statusCode" : 403,
+						"statusMessage" : "Email Already Exists",
+						"results" : results
+				};			
+			} else {
+				console.log("failureeeeeeeeeeeeeeeeeeeeeeee");
+				json_response ={
+						"statusCode" : 200,
+						"statusMessage" : "Doesnot Exist",
+						"results" : results
+				};
+			}
+			callback(null, json_response);
+		});
+	});	*/
 
-			var insertQuery = "insert into amazonfresh.farmer (farmer_id, firstname, lastname, address, city, " +
-			"state, zip_code, phone_number, email, password, approved) values(?,?,?,?,?,?,?,?,?,?)";
-
-			console.log(query);
-			var inputParams = [id,firstName,lastName,streetAddress,city,state,zipCode,phoneNumber,email,password, approved];
-			console.log(inputParams);
-
-			var insertQuery = mysqlModule.format(insertQuery, inputParams);
-
-			mysql.fetchData(function(err,results){
-				console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-				console.log(err);
-				console.log(results);		
-
-				if(err){
-					console.log("//////////////////");
-					console.log(err.code);
-					json_response ={
-							"statusCode" : 401,
-							"statusMessage" : err.code
-					};
-					callback(null, json_response);
-				}
-				else 
-				{
-					mongo.connect(mongoURL, function(){
-						var coll = mongo.collection('farmer_details');
-						coll.insertOne({farmer_id: id,image:image,video:video,description:description}, function(err, results){
-
-							console.log(results);
-							if (err) 
-							{
-								console.log("////////////////// mongo error");
-								json_response ={
-										"statusCode" : 401,
-										"statusMessage" : "Unexpected error occured"
-								};
-								callback(null, json_response);
-							} else {
-								console.log("////////////////// mongo success");
-								json_response ={
-										"statusCode" : 200,
-										"statusMessage" : "Signup Successful"
-								};
-								callback(null, json_response);
-							}							
-						});
-					});
-				}  
-			},insertQuery);
-		}
-	}, query);
+	json_response ={
+			"statusCode" : 200,
+			"statusMessage" : "Signup successful"
+	};
+	callback(null, json_response);
 };
 
 exports.handle_deleteAccount_request = function(msg, callback){
-	var json_response = {};
-	var id = msg.id;
-	var category = msg.category;
-	var approved = 3;
 
-	var query = "";
+};
 
-	if(category == "farmer")
-	{
-		query = "update amazonfresh.farmer set approved='"+ approved +"' where farmer_id ='"+ id +"'";
-	}
-	else 
-	{
-		query = "update amazonfresh.customer set approved='"+ approved +"' where customer_id ='"+ id +"'";
-	}
-
-	mysql.fetchData(function(error, results) {
-		if(error)
-		{
-			json_response ={
-					"statusCode" : 401,
-					"results" : error.code
-			};
-			callback(null, json_response);
-		}
-		else
-		{	
-			json_response ={
-					"statusCode" : 403,
-					"statusMessage" : "No pending requests"
-			};
-			callback(null, json_response);			
-		}		
-	}, query);
+function getRandomInt(min, max){
+	return Math.floor(Math.random() * (max - min + 1)) + min;
 };
