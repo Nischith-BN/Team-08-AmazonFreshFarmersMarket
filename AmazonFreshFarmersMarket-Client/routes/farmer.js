572b6cd6d14117c81d640a20da7f5062ce1e2988
jainbhargav <jainbhargav01@gmail.com>
2016-04-26 02:24:06 -0700

    Revert "Product and Farmer API Committed"
    This reverts commit f3abb5d44e6d303199e1bbd03b476ccf4bbbfd45.

--- AmazonFreshFarmersMarket-Client/routes/farmer.js
+++ AmazonFreshFarmersMarket-Client/routes/farmer.js
@@ -17,28 +17,10 @@ exports.listFarmers = function(req,res){
 };
 
 exports.amendFarmerDetails = function(req,res){
-	var farmerId ="100000011"; //req.param("farmerId");
-	var firstName ="Fname first"; //req.param("firstName");
-	var lastName ="Lname last";//req.param("flastName");
-	var address ="201 S street";//req.param("address");
-	var city ="San Jose city";//req.param("city");
-	var state ="CA state";//req.param("state");
-	var zipCode ="95112";//req.param("zipCode");
-	var phoneNumber ="40864633354";//req.param("phoneNumber");
-	var password ="password";//req.param("password");
-	//req.param("productDescription");
+	var farmerId = req.param("farmerId");
 
 	var msg_payload = { 
-			"farmerId" : farmerId,
-			"firstName" : firstName,
-			"lastName" : lastName,
-			"address" : address,
-			"city" : city,
-			"state" : state,
-			"zipCode" : zipCode,
-			"phoneNumber" : phoneNumber,
-			"password" : password
-			
+			"searchString" : searchString
 	};
 
 	mq_client.make_request('amendFarmerDetails_queue',msg_payload, function(err,results){
@@ -71,10 +53,12 @@ exports.searchFarmer = function(req,res){
 };
 
 exports.fetchFarmerDetails = function(req,res){
-	var farmerId = "100000011";//req.param("farmerId");
+	var farmerId = req.param("farmerId");
+
 	var msg_payload = { 
 			"farmerId" : farmerId
 	};
+
 	mq_client.make_request('fetchFarmerDetails_queue',msg_payload, function(err,results){
 		if(err){
 			throw err;
